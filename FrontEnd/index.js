529486998f852ea4437a741cf1edd58c72af82d8
BttCrtd <courtadon.baptiste@gmail.com>
2026-02-27 14:35:37 +0100

    Filtrage des projets dans le dom

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -21,7 +21,7 @@ function projectSheet(idProject, listProject) {
 }
 
 // Affichage des fiches projets
-async function generateProjects() {
+async function generateProjects(idcategorie) {
   const containerProjects = document.querySelector(".gallery");
   containerProjects.innerHTML = "";
   
@@ -33,8 +33,16 @@ async function generateProjects() {
       .then((listProject) => {
         // Création de toutes les fiches projets //
           for (const project of listProject) {
-            const myProject = projectSheet(project.id, listProject);
-            containerProjects.appendChild(myProject);
+            if (idcategorie){
+              if (project.categoryId === idcategorie) {
+                const myProject = projectSheet(project.id, listProject);
+                containerProjects.appendChild(myProject);
+              };
+            } else {
+                const myProject = projectSheet(project.id, listProject);
+                containerProjects.appendChild(myProject);
+              
+            }
           }
         } 
       );
@@ -47,3 +55,49 @@ async function generateProjects() {
 }
 
 generateProjects();
+
+
+/* ----------  Catégories de projets  ---------- */
+/* Création d'un bouton de filtrage de catégorie de projet */
+function categorieButton(categoryName, categoryID = null, isActive = false) {
+    const categoriesContainer = document.querySelector(".categories-container");
+    const buttonCategory = document.createElement("button");
+    buttonCategory.innerText = categoryName;
+
+    if (categoryID !== null) {
+      buttonCategory.id = categoryID;
+    }
+    
+    if (isActive) {
+      buttonCategory.classList.add("active");
+    }
+
+    buttonCategory.addEventListener("click", () => {
+      const buttons = categoriesContainer.querySelectorAll("button");
+      buttons.forEach((button) => button.classList.remove("active"));
+      buttonCategory.classList.add("active");
+      generateProjects(Number(buttonCategory.id))
+    });
+    categoriesContainer.appendChild(buttonCategory);
+  }
+
+/* Récupération et affichaches des catégories de projets */
+async function getCategoriesProject() {
+  try {
+    await fetch("http://localhost:5678/api/categories")
+      .then((response) => {
+        return response.json();
+      }) 
+      .then((listCategories) => {
+        categorieButton("Tous",null, true);
+        for (const category of listCategories) {
+          categorieButton(category.name, category.id);
+        }
+        });
+      ;
+  } catch (error) {
+    console.log("Une erreur est survenue lors du chargement des catégories de projets", error);
+  }
+}
+
+getCategoriesProject();
